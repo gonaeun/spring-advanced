d4bda62105040799cb89933c0d9c38e31279ac91
고나은 <gonaeunn@gmail.com>
2025-04-20 19:11:05 +0900

    fix: 레벨 3-1 테스트코드 수정
    테스트코드 정상적으로 작동 확인함

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -15,14 +15,14 @@ class PasswordEncoderTest {
 
     @Test
     void matches_메서드가_정상적으로_동작한다() {
-        // given
+        // given 준비 : 비밀번호 암호화
         String rawPassword = "testPassword";
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
-        // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        // when 행동 : 비밀번호 일치하는지 검사
+        boolean matches = passwordEncoder.matches(rawPassword, encodedPassword); // 파라미터 순서 올바르게 변경
 
-        // then
+        // then 검증 : 일치한다면 true
         assertTrue(matches);
     }
 }
